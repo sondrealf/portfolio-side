0d0382f8de70b7042f5d44244b5113227ac83f14
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-05-27 00:34:42 +0200

    portfolio

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'next-themes';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider>
+      <Navigation />
+      <Component {...pageProps} />
+      <Footer />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
