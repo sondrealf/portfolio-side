b029d96ed6d22e23b075923b6fbf33f586c18191
Sondre Alfnes <sondralf@stud.ntnu.no>
2022-07-17 23:59:12 +0200

    github and live at md

--- components/Project.tsx
+++ components/Project.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import Icon from './Icon'
 import Card from './Card'
+import { FaGlobe, FaGithub } from 'react-icons/fa/'
 
 const Project = (obj: any) => {
   return (
@@ -16,7 +17,31 @@ const Project = (obj: any) => {
           githubLink={obj.source}
           liveLink={obj.live}
         />
+        <div className="mt-5 flex justify-evenly text-white dark:text-black md:hidden">
+          {obj.source == '' ? (
+            <></>
+          ) : (
+            <a
+              href={obj.source}
+              className="my-auto cursor-pointer hover:text-slate-400"
+            >
+              <FaGithub className="mx-auto text-5xl" />
+              <p className="font-bold">View Source</p>
+            </a>
+          )}
 
+          {obj.live == '' ? (
+            <></>
+          ) : (
+            <a
+              href={obj.live}
+              className="my-auto cursor-pointer hover:text-slate-400"
+            >
+              <FaGlobe className="mx-auto text-5xl" />
+              <p className="font-bold">View Live Version</p>
+            </a>
+          )}
+        </div>
         <div className="w-full md:w-1/2">
           <h1 className="mt-8 text-center text-2xl font-bold text-blue-400">
             {obj.subheader}
