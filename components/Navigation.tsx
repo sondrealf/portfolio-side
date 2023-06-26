25c460b2e50c35ff462a9b5f9c4ba4b36ac721f6
Sondre Alfnes <sondre.alfnes@gmail.com>
2023-06-27 01:25:35 +0200

    fix

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,13 +6,10 @@ const Navigation = () => {
   return (
     <div className="animate-navOpen sticky top-0 z-20 mb-6 bg-black bg-opacity-40 py-6 dark:bg-gray-400 dark:bg-opacity-40 ">
       <div className="container mx-auto flex items-center justify-between px-4 lg:max-w-4xl">
-        <Link
-          href="/"
-          className={
-            'font-medium uppercase tracking-wider text-white transition-colors hover:text-sky-500 dark:text-gray-900'
-          }
-        >
-          Sondre Alfnes
+        <Link href="/">
+          <p className="font-medium uppercase tracking-wider text-white transition-colors hover:text-sky-500 dark:text-gray-900">
+            SONDRE ALFNES
+          </p>
         </Link>
 
         <ThemeSwitch />
